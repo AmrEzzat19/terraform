70effe46c8d779c38d9b50ced7c609f110ece0f1
Amr Ezzat <63130834+AmrEzzat19@users.noreply.github.com>
2023-09-11 13:32:46 +0300

    Update provider.tf

--- task/provider.tf
+++ task/provider.tf
@@ -1,12 +1,12 @@
 provider "oci"{
     region  =  var.region 
-    fingerprint = var.fingerprint
     profile = "DEFAULT"
     /*created ~/.osi/config that contains private information
         [DEFAULT]
         tenancy_ocid = "YOUR_TENACITY_OCID"
         user_ocid = "YOUR_USER_OCID
         private_key_path = "YOUR_PRIVATE_KEY_PATH"  
+        fingerprint = "YOUR_FINGERPRINT"
     */
 }   
 
